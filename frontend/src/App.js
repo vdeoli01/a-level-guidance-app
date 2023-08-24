7c8d6b6cd9c8534149e30d02474e6bbfbdbc64bd
vdeoli01 <vdeoli01@student.bbk.ac.uk>
2023-08-24 07:58:59 +0100

    #19 Updating front end env variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ const BASE_API_ENDPOINT = process.env.REACT_APP_BASE_API_ENDPOINT;
 
 function App() {
   const [message, setMessage] = useState('');
-  console.log(process.env.REACT_APP_BASE_API_ENDPOINT);
   console.log(process.env.NODE_ENV)
   useEffect(() => {
     fetch(BASE_API_ENDPOINT)  // Adjust the URL to point to your backend endpoint.
@@ -13,10 +12,13 @@ function App() {
         .then(data => setMessage(data.message));
   }, []);
 
+  console.log("Fetched message:", message); // TODO This is undefined, fix
+
   return (
       <div className="App">
         <header className="App-header">
-          {message}
+            <p>{message}</p>
+            <p>Hello World</p>
         </header>
       </div>
   );
